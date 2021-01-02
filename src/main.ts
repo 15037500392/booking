d041362b514d9eb23dcb0cd42e2adc49d8f60f7c
zxx <ss15037500392@163.com>
2021-01-02 16:25:16 +0800

    init

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import '@/common/css/common.css'
 Vue.config.productionTip = false
 
 new App().$mount()
---
+++ src/pages/views/home/index.vue
@@ -0,0 +1,17 @@
+<template>
+	<view>
+		{{name}}
+	</view>
+</template>
+
+<script lang="ts">
+	import Vue from 'vue'
+	import Component from 'vue-class-component'
+	@Component({})
+	export default class Home extends Vue{
+		private name:string = '张三'
+	}
+</script>
+
+<style lang="less" scoped>
+</style>
