7222ccd567c0af64c588b63094da4fb25727d930
shanyao <ss15037500392@163.com>
2021-06-28 15:57:11 +0800

    pr第一次尝试

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,18 @@
         onShow() {
             console.log('App Show')
         },
-        onHide() {
+        onHide(e) {
             console.log('App Hide')
-        }
+          if (e.wheelDelta) {
+            //判断浏览器IE，谷歌滑轮事件
+            this.$store.commit('common/SET_WHEEL_DELTA', e.wheelDelta)
+          } else if (e.detail) {
+            //Firefox滑轮事件
+            this.$store.commit('common/SET_WHEEL_DELTA', e.detail)
+          }
+          this.$store.commit('common/SET_SCROLL_DISTANCE', e.target.scrollTop)
+        },
+
     });
 </script>
 
