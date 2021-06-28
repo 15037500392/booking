0244ecc55e5929458473760c7a3c17f805c3dbb7
shanyao <ss15037500392@163.com>
2021-06-28 16:20:17 +0800

    pr后的代码

--- src/App.vue
+++ src/App.vue
@@ -7,18 +7,10 @@
         },
         onShow() {
             console.log('App Show')
+
         },
         onHide(e) {
-            console.log('App Hide')
-          if (e.wheelDelta) {
-            //判断浏览器IE，谷歌滑轮事件
-            this.$store.commit('common/SET_WHEEL_DELTA', e.wheelDelta)
-          } else if (e.detail) {
-            //Firefox滑轮事件
-            this.$store.commit('common/SET_WHEEL_DELTA', e.detail)
-          }
-          this.$store.commit('common/SET_SCROLL_DISTANCE', e.target.scrollTop)
-        },
+        }
 
     });
 </script>
